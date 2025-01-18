09b5475340eac61f8bb9eb1eaaf7fc1a03102c3b
Slonbax <thinotrandri@gmail.com>
2025-01-18 10:23:31 +0100

    ajout responsive mobile

---
+++ index.js
@@ -0,0 +1,9 @@
+
+if (window.innerWidth <= 768) {
+    window.location.href = './mobile/index-mobile.html';
+    window.location.href = './phone.css';
+} else {
+    window.location.href = 'index.html'
+    window.location.href = 'style.css'
+}
+
